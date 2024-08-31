a3010a4080c341fee665a12d27268a1302598d1f
reza <rezaglmy@gmail.com>
2024-08-31 19:26:35 +0330

    clean up

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
-import './index.css';
 import { Provider } from 'react-redux';
 import { store } from './store/index.ts';
 import { StyledEngineProvider, ThemeProvider } from '@mui/material/styles';
--- src/theme/index.tsx
+++ src/theme/index.tsx
@@ -2,20 +2,6 @@ import { createTheme } from '@mui/material/styles';
 
 const theme = createTheme({
   cssVariables: true,
-  // palette: {
-  //   // primary: {
-  //   //   light: '#a27cf7',
-  //   //   main: '#8b5cf6',
-  //   //   dark: '#6140ac',
-  //   //   contrastText: '#fff',
-  //   // },
-  //   secondary: {
-  //     light: '#90a4ae',
-  //     main: '#607d8b',
-  //     dark: '#455a64',
-  //     contrastText: '#000',
-  //   },
-  // },
   breakpoints: {
     values: {
       xs: 0,
